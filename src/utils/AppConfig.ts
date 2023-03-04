4ac5b9042f322fa2b95e0fc6c0bc5c203fecbc5b
Michael Maust <info@michaelmaust.com>
2023-03-04 00:05:16 -0600

    Init

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,8 @@
+// FIXME: Update this configuration file based on your project information
+
+export const AppConfig = {
+  site_name: 'Starter',
+  title: "Michael Maust's Portfolio",
+  description: 'Portfolio site',
+  locale: 'en',
+};
